02dbee3e46d31199c67c639e07a8a4b0b48604fb
leo25788 <huyfantasy257@gmail.com>
2025-08-03 16:47:35 +0700

    Bai tap 4

--- tests/lesson-5/test3.spec.ts
+++ tests/lesson-5/test3.spec.ts
@@ -2,14 +2,31 @@ import { test } from "@playwright/test";
 
 
 test('Add 100 todos and delete odd todos', async ({ page }) => {
+    let test = [];
+    let xpathtest;
+    let xoa = '//*[@id="1-delete"]';
+
 
 
     await page.goto('https://material.playwrightvn.com/');
     await page.click('//*[@id="section-xpath"]/table/tbody/tr[3]/td[2]/a');
-    for (let i = 1; i <= 100; i++) {
+    
+
+    for (let i = 1; i <= 10; i++) {
         await page.locator('//*[@id="new-task"]').fill(`Todo ${i}`);
         await page.locator('//*[@id="add-task"]').click();
+
+         /* if ((i) % 2 !== 0) {
+
+
+        xpathtest = '//*[@id="' + i + '-delete"]';
+        console.log(xpathtest);
+        await page.locator(xpathtest).click();
+
+
+    } */
     }
+    await page.locator('//*[@id="1-delete"]').click();
 
 
 });
