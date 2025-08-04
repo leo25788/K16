a8773b07a46da66b8aac504a0b9aa27f47f9bc50
leo25788 <huyfantasy257@gmail.com>
2025-08-04 11:38:02 +0700

    Update bai tap 3

--- tests/lesson-5/test3.spec.ts
+++ tests/lesson-5/test3.spec.ts
@@ -1,34 +1,47 @@
 import { test } from "@playwright/test";
 
 
-test('Add record', async ({ page }) => {
+test('Add record', async ({ page }) =>
+     {
     let test = [];
-    let xpathtest;
+    let xpathtest = [];
     let xoa = '//*[@id="1-delete"]';
 
 
 
     await page.goto('https://material.playwrightvn.com/');
     await page.click('//*[@id="section-xpath"]/table/tbody/tr[3]/td[2]/a');
-    
+
 
     for (let i = 1; i <= 10; i++) {
         await page.locator('//*[@id="new-task"]').fill(`Todo ${i}`);
         await page.locator('//*[@id="add-task"]').click();
+         if ((i) % 2 !== 0) {
+        xpathtest.push(`//*[@id="todo-${i}-delete"]`);
+        console.log(xpathtest);
+        xpathtest.reverse();
+        console.log(xpathtest);
 
-          if ((i) % 2 !== 0) {
+  }
+  // xoá 
+  for (const xpath of xpathtest) {
+  // Chấp nhận dialog nếu hiện
+  page.on('dialog', dialog => dialog.accept());
+
+ 
+
+  // Click button theo XPath
+  await page.locator(xpath).click();
+}
 
 
-        xpathtest = '//*[@id="todo-' + i + '-delete"]';
-        //console.log(xpathtest);
-        page.on('dialog', dialog => dialog.accept());
-        await page.locator(xpathtest).click();
+
+        
 
 
-    } 
+        }
     }
    
 
-
-});
+);
 
